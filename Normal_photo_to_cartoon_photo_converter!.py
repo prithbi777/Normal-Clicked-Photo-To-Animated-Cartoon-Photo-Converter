acd9eaa33f7e79a9be7aef36e477f2067c48ea2b
Prithbiraj Mahanta <90209139+factorprithy@users.noreply.github.com>
2022-01-30 17:59:51 +0530

    Add files via upload

---
+++ Normal_photo_to_cartoon_photo_converter!.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+entered_path = input("Please enter the path of the img : ")
+path = (entered_path.replace("\\", "\\\\")).replace("\"", "")
+img = cv2.imread(path)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color,color,mask=edges)
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
